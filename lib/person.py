2a5c8b38e7ff190b7edc1f5272257cde6bac5548
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-02-04 12:55:27 -0500

    completed lab

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,7 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+     def talk(self):
+       print("Hello World!")
+     def walk(self):
+       print("The person is walking.")
